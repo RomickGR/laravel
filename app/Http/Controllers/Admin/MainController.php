cd385a490cbfebd7bd09d1cc8e922e5a9cec7273
RomickGR <RomickGR@mail.ru>
2021-02-08 15:00:17 +0700

    Add files via upload

---
+++ app/Http/Controllers/Admin/MainController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+//use App\Models\Tag;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class MainController extends Controller
+{
+    //
+
+    public function index(){
+
+        /*
+Tag::create([
+    'title'=>'Привет мир!',
+]);
+
+       */
+
+        /*
+$tag = new Tag();
+$tag->title = 'Привет мир!';
+$tag->save();
+        */
+
+    return view('admin.index');
+
+    }
+
+}
