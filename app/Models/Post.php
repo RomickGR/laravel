9a76aaa21334d0f501956ef68f7dfb4ae470445e
RomickGR <RomickGR@mail.ru>
2021-02-08 15:03:59 +0700

    Add files via upload

---
+++ app/Models/Post.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+//use http\Env\Request;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
+
+
+class Post extends Model
+{
+    use Sluggable;
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'content', 'category_id', 'thumbnail'];
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public static function uploadImage(Request $request, $image = null)
+    {
+        if ($request->hasFile('thumbnail')) {
+            if ($image) {
+                Storage::delete($image);
+            }
+            $folder = date('Y-m-d');
+            return $request->file('thumbnail')->store("images/{$folder}");
+        }
+        return null;
+    }
+
+
+    public function getImage()
+    {
+        if (!$this->thumbnail) {
+            return asset("no-image.png");
+        }
+
+        return asset("uploads/{$this->thumbnail}");
+    }
+}
